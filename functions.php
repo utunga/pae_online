8878514ff1f1b8d6571fc3c65084a964ca7a92f5
MIles Thompson <utunga@gmail.com>
2018-09-10 17:12:24 +1200

    remove 'no plugin sitemap.xml' which totally didnt work

--- functions.php
+++ functions.php
@@ -117,42 +117,6 @@ add_theme_support( 'html5', array(
 ) );
 
 
-add_action( 'publish_post', 'itsg_create_sitemap' );
-add_action( 'publish_page', 'itsg_create_sitemap' );
-
-function itsg_create_sitemap() {
-
-    $postsForSitemap = get_posts(array(
-        'numberposts' => -1,
-        'orderby' => 'modified',
-        'post_type'  => array( 'post', 'page', 'listing', 'event' ),
-        'order'    => 'DESC'
-    ));
-
-    $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
-    $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-
-    foreach( $postsForSitemap as $post ) {
-        setup_postdata( $post );
-
-        $postdate = explode( " ", $post->post_modified );
-
-        $sitemap .= '<url>'.
-          '<loc>' . get_permalink( $post->ID ) . '</loc>' .
-          '<lastmod>' . $postdate[0] . '</lastmod>' .
-          '<changefreq>monthly</changefreq>' .
-         '</url>';
-      }
-
-    $sitemap .= '</urlset>';
-
-    $fp = fopen( ABSPATH . 'sitemap.xml', 'w' );
-
-    fwrite( $fp, $sitemap );
-    fclose( $fp );
-}
-
-
 //// Enable support for post formats.
 //add_theme_support( 'post-formats', array(
 //    'aside',
